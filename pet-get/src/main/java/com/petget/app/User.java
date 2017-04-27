3b6a658c34ab946080a2aec9da81e2d44c3f7e50
HunterHaskins <huha8075@colorado.edu>
2017-04-27 17:57:37 -0600

    adding user class skeleton file

---
+++ pet-get/src/main/java/com/petget/app/User.java
@@ -0,0 +1,17 @@
+package com.petget.app;
+
+/**
+ * Created by Hunter on 4/27/2017.
+ */
+public class User {
+
+    public int id;
+    public String name;
+
+    private String password;
+
+    public boolean testPassword(String test){}
+
+
+
+}
