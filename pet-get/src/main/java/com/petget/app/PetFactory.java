602aba1df5d1c42ec576d91ed9e3027e11183358
HunterHaskins <huha8075@colorado.edu>
2017-04-28 17:42:41 -0600

    updated the pet factory class

--- pet-get/src/main/java/com/petget/app/PetFactory.java
+++ pet-get/src/main/java/com/petget/app/PetFactory.java
@@ -3,6 +3,32 @@ package com.petget.app;
 /**
  * Created by Michael on 4/27/2017.
  */
-public interface PetFactory {
-    public Pet createPet();
+public abstract class PetFactory {
+    public Pet createPet(String type){
+        Pet p = null;
+
+        if(type.equals("Bird")){
+            p = new Bird();
+
+        } else if(type.equals("Dog")){
+            p = new Dog();
+
+        } else if(type.equals("Cat")){
+            p = new Cat();
+
+        } else if(type.equals("Snakes")){
+            p = new Snakes();
+
+        } else if(type.equals("Hamster")){
+            p = new Hamster();
+
+        } else if(type.equals("Spider")){
+            p = new Spider();
+
+        } else if(type.equals("Fish")){
+            p = new Fish();
+        }
+
+        return p;
+    }
 }
