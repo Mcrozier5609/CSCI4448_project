d19576d9821f29f766790cc84c4acc89cdde7d83
HunterHaskins <huha8075@colorado.edu>
2017-04-27 18:19:49 -0600

    Adding more skeleton classes

---
+++ pet-get/src/main/java/com/petget/app/Shelter.java
@@ -0,0 +1,24 @@
+package com.petget.app;
+
+/**
+ * Created by Hunter on 4/27/2017.
+ */
+public class Shelter {
+
+    public Pet[] pets = new Pet[100];
+    public AdoptionRequest[] requests = new AdoptionRequest[50];
+
+
+    public Pet addPet(Pet p){}
+
+    public Pet remove(Pet p){}
+
+    public Pet[] getAllPets(){}
+
+    public void addRequest(AdoptionRequest ar){}
+
+    public AdoptionRequest getRequests(){}
+
+    public void addAppointment(Appointment a){}
+
+}
