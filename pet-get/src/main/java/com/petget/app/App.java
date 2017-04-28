ad59393fe00c1645cec668df2818f46fbed14b20
719Ben <ben.williams@colorado.edu>
2017-04-27 19:14:13 -0600

    Added second frontend page

--- pet-get/src/main/java/com/petget/app/App.java
+++ pet-get/src/main/java/com/petget/app/App.java
@@ -4,6 +4,22 @@ import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
-        get("/", (req, res) -> "Hello World");
+        get("/", (req, res) -> renderContent("index.html"));
     }
+
+    private String renderContent(String htmlFile) {
+        try {
+            //get the url class resource
+            URL url = getClass().getResource(htmlFile);
+
+            // Return a String which has all
+            // the contents of the file.
+            Path path = Paths.get(url.toURI());
+            return new String(Files.readAllBytes(path), Charset.defaultCharset());
+        } catch (IOException | URISyntaxException e) {
+            // Add your own exception handlers here.
+        }
+        return null;
+    }
+
 }
