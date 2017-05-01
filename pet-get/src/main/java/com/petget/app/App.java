d837b2fd0627b60b43be0c6bf0141552746fc287
719Ben <ben.williams@colorado.edu>
2017-04-30 23:13:14 -0600

    Added better templating

--- pet-get/src/main/java/com/petget/app/App.java
+++ pet-get/src/main/java/com/petget/app/App.java
@@ -1,111 +1,77 @@
 package com.petget.app;
 
-import java.sql.*;
 import static spark.Spark.*;
-import java.io.*;
 import java.util.Map;
 import java.util.HashMap;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
+import spark.ModelAndView;
+import spark.template.handlebars.*;
 
 public class App {
-    private static final Map<String, Object> settings = new HashMap<String, Object>();
-
-    public static String parse(String pattern, String text, Map<String, Object> locals) {
-        Matcher regexp = Pattern.compile(pattern).matcher(text);
-        while (regexp.find()) {
-            text = regexp.replaceFirst(locals.get(regexp.group(1)).toString());
-        }
-        return text;
-    }
-
-    private static String parseFile(String file, String pattern, Map<String, Object> locals) {
-        StringBuffer content = new StringBuffer("");
-        try {
-            BufferedReader buffer = new BufferedReader(new FileReader(file));
-            String line = null;
-
-            while ((line = buffer.readLine()) != null) {
-                content.append(parse(pattern, line, locals) + "\n");
-            }
-
-            buffer.close();
-        }
-        catch (Exception exception) {
-            System.out.printf("ERROR: %s\n", exception.getMessage());
-        }
-        finally {
-            return content.toString();
-        }
-    }
-
-    public static String render(String file, Map<String, Object> locals) {
-        return layout(file, parseFile(("src/main/resources/" + file), "\\{\\{(.*?)\\}\\}", locals));
-    }
-
-    public static String layout(String file, String content) {
-        HashMap<String, Object> layout = new HashMap<String, Object>();
-        layout.put("content", content);
-        return parseFile("src/main/resources/layout.html", "\\{\\{(content)\\}\\}", layout);
-    }
-
-    public static void set(String key, Object value) {
-        settings.put(key, (String) value);
-
-    }
-
-    public static Object settings(String key) {
-        return settings.get(key);
-    }
-
-
     public static void main(String[] args) {
         port(8000);
         staticFileLocation("/");
+
+        //route for the index view
         get("/", (req, res) -> {
-                set("title", "Home");
-                return render("index.html", settings);
-        });
+            Map template_map = new HashMap();
+            String session_id = req.session().attribute("id");
+
+            if (session_id != null) {
+                template_map.put("logged_in", "true");
+            }
+
+            return new ModelAndView(template_map, "index.html");
+        }, new HandlebarsTemplateEngine());
+
+        //route for login view
         get("/login", (req, res) -> {
-            set("title", "Login");
+            Map template_map = new HashMap();
             String session_id = req.session().attribute("id");
 
             if (session_id != null) {
                 res.redirect("/pets");
-                return "";
+                return new ModelAndView(null, null);
             }
 
-            return render("login.html", settings);
-        });
+            return new ModelAndView(template_map, "login.html");
+        }, new HandlebarsTemplateEngine());
+
+        //route for login POST request
         post("/login/process", (req, res) -> {
+            Map template_map = new HashMap();
             String email = req.queryParams("email");
             String raw_password = req.queryParams("password");
 
+            //get the new user
             User login_user = new User(email);
 
             if(login_user.getPasswordHash() == null) {
-                return "User does not exist";
+                template_map.put("error", "User does not exist");
+                return new ModelAndView(template_map, "error.html");
             }
 
             if(login_user.testPassword(raw_password)) {
                 req.session().attribute("id", login_user.email);
                 res.redirect("/pets");
-                return "";
+                return new ModelAndView(null, null);
             }
-            return "Invalid password for user";
+            template_map.put("error", "Incorrect password");
+            return new ModelAndView(template_map, "error.html");
 
-        });
+        }, new HandlebarsTemplateEngine());
+
+        //route for pets view
         get("/pets", (req, res) -> {
-            set("title", "Pets");
+            Map template_map = new HashMap();
             String session_id = req.session().attribute("id");
 
             if (session_id == null) {
                 res.redirect("/login");
-                return "";
+                return new ModelAndView(null, null);
             }
 
-            return render("petList.html", settings);
-        });
 
+            return new ModelAndView(template_map, "petList.html");
+        }, new HandlebarsTemplateEngine());
     }
 }
